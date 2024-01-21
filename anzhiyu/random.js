35532ba951543be01860a142253a4711688a5386
U0o0U <1219684158@qq.com>
2024-01-21 16:46:54 +0000

    github action update Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
 var posts=["2024/01/16/Detecting Unknown Encrypted Malicious Traffic in Real Time via Flow Interaction Graph Analysis 论文精读/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+    window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
