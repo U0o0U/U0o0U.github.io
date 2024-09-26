1b560269fb68a15129c9dc16d9ffd7edd1f2b442
U0o0U <1219684158@qq.com>
2024-09-26 10:11:44 +0000

    twikoo region Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
 var posts=["2024/01/16/Hello world/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+    window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
